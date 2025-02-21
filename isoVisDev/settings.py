a9cbe8d1fe2d03ef2070d24d8124556ca6863495
SziKayLeung <szikayleung@gmail.com>
2025-02-21 13:42:40 +0000

    add additional enivornments to hosts

--- isoVisDev/settings.py
+++ isoVisDev/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-t-n*(rbzl@29toox-%zh16z^$8jnslm4kddj*x0pazut#y9t!h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['isoforms.com','www.isoforms.com','127.0.0.1','172.31.19.167','isoVis-deploy-env.eba-r7r9zsja.eu-north-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['isoforms.com','www.isoforms.com','127.0.0.1','172.31.19.167','isoVis-deploy-env.eba-r7r9zsja.eu-north-1.elasticbeanstalk.com',
+'isovisdev-env2102test.eba-bkixdg3p.eu-north-1.elasticbeanstalk.com']
 
 
 # Application definition
